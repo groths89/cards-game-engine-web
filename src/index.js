69611f43d98be4a2ff389027d37e75676a0ce186
Gregory Paul Rothstein <g.p.rothstein@gmail.com>
2025-07-25 12:05:40 -0400

    Fix: Adjust frontend build for deployment

--- src/index.js
+++ src/index.js
@@ -7,12 +7,10 @@ import reportWebVitals from './reportWebVitals';
 import { GameProvider } from './contexts/GameContext';
 import { NotificationsProvider } from './contexts/NotificationsContext';
 import { AuthProvider } from './contexts/AuthContext';
-import { AmplifyOutputs } from '@aws-amplify/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AmplifyOutputs />
     <BrowserRouter>
       <AuthProvider>
         <GameProvider>
