28f2d84ef78a3efe3b215505d714b09120a38908
Gregory Paul Rothstein <g.p.rothstein@gmail.com>
2025-07-25 12:02:23 -0400

    Fix: Adjust frontend build for amplify_outputs.json import

--- src/index.js
+++ src/index.js
@@ -7,14 +7,12 @@ import reportWebVitals from './reportWebVitals';
 import { GameProvider } from './contexts/GameContext';
 import { NotificationsProvider } from './contexts/NotificationsContext';
 import { AuthProvider } from './contexts/AuthContext';
-import { Amplify } from 'aws-amplify';
-import outputs from './amplify_outputs.json';
-
-Amplify.configure(outputs);
+import { AmplifyOutputs } from '@aws-amplify/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AmplifyOutputs />
     <BrowserRouter>
       <AuthProvider>
         <GameProvider>
