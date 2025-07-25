2fa98253559fa5cddd29ef312e95896aecc7e5b2
Gregory Paul Rothstein <g.p.rothstein@gmail.com>
2025-07-25 17:31:31 -0400

    Fix userPool string

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,13 +1,6 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
 const schema = a.schema({
-  // Existing Todo model
-  Todo: a
-    .model({
-      content: a.string(),
-    })
-    .authorization((allow) => [allow.guest()]),
-
   // Users table
   User: a
     .model({
@@ -24,8 +17,8 @@ const schema = a.schema({
     })
     .identifier(['userId'])
     .authorization((allow) => [
-      allow.owner().to(['read', 'update']),
-      allow.authenticated().to(['read']),
+      allow.owner('userPools').to(['read', 'update']),
+      allow.authenticated('userPools').to(['read']),
       allow.guest().to(['read'])
     ]),
 
@@ -44,8 +37,8 @@ const schema = a.schema({
     })
     .identifier(['gameId', 'userId'])
     .authorization((allow) => [
-      allow.owner().to(['read', 'create']),
-      allow.authenticated().to(['read']),
+      allow.owner('userPools').to(['read', 'create']),
+      allow.authenticated('userPools').to(['read']),
     ]),
 });
 
@@ -60,33 +53,3 @@ export const data = defineData({
     },
   },
 });
-
-/*== STEP 2 ===============================================================
-Go to your frontend source code. From your client-side code, generate a
-Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
-WORK IN THE FRONTEND CODE FILE.)
-
-Using JavaScript or Next.js React Server Components, Middleware, Server 
-Actions or Pages Router? Review how to generate Data clients for those use
-cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
-=========================================================================*/
-
-/*
-"use client"
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-
-const client = generateClient<Schema>() // use this Data client for CRUDL requests
-*/
-
-/*== STEP 3 ===============================================================
-Fetch records from the database and use them in your frontend component.
-(THIS SNIPPET WILL ONLY WORK IN THE FRONTEND CODE FILE.)
-=========================================================================*/
-
-/* For example, in a React component, you can use this snippet in your
-  function's RETURN statement */
-// const { data: todos } = await client.models.Todo.list()
-
-// return <ul>{todos.map(todo => <li key={todo.id}>{todo.content}</li>)}</ul>
-
